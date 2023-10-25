03c102e647f6c13ca433ed62ff30f5fe31230ded
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2023-10-25 10:51:59 -0500

    chore: fix 8

--- src/services/news.ts
+++ src/services/news.ts
@@ -1,6 +1,13 @@
-import { fecthBackendAPI } from "@/lib/fetchBackendAPI"
+import { fecthBackendAPI } from "@/lib/fetchBackendAPI";
 import type { News, ResponseAPI } from "@/models";
 
-export const getTidings = async (params?: string[]): Promise<ResponseAPI<News[]>> => await fecthBackendAPI<News[]>('/tidings', params)
+export const getTidings = async (
+  params?: string[]
+): Promise<ResponseAPI<News[]>> =>
+  await fecthBackendAPI<News[]>("/tidings", params);
 
-export const getNews = async (id: string, params?: string[]): Promise<ResponseAPI<News>> => await fecthBackendAPI<News>(`/people/${id}`, params)
+export const getNews = async (
+  id: string,
+  params?: string[]
+): Promise<ResponseAPI<News>> =>
+  await fecthBackendAPI<News>(`/tidings/${id}`, params);
