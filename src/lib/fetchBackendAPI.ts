7c4d6de3e073aedc7af2e74a70b2a7f4e2693b7c
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2023-10-25 12:29:22 -0500

    chore: fix 16

--- src/lib/fetchBackendAPI.ts
+++ src/lib/fetchBackendAPI.ts
@@ -2,17 +2,20 @@ import { BACKEND_URL } from "@/lib/constants";
 import type { ResponseAPI } from "@/models";
 
 export const fecthBackendAPI = <T>(
-  url: string,
+  endpoint: string,
   params?: string[],
   options?: RequestInit
 ): Promise<ResponseAPI<T>> => {
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), 5000);
-  const requestURL = BACKEND_URL.concat(
-    "/api",
-    url,
-    params ? `?${params.join("&")}` : ""
-  );
+  const url = new URL(`${BACKEND_URL}/api${endpoint}`);
+  if (params) {
+    Object.entries(params).map(([key, value]) => {
+      url.searchParams.append(key, value);
+    });
+  }
+  const requestURL = url.toString();
+
   return fetch(requestURL, { signal: controller.signal, ...options })
     .then((res) => {
       if (!res.ok) throw new Error(res.statusText);
