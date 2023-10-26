b34fd4dd57ab6a48cc5d81fb1d031207931ad4c6
Jhon Romero <48453401+JhonRomero26@users.noreply.github.com>
2023-10-26 21:34:14 +0000

    fix: env variables

--- src/lib/constants/environment.ts
+++ src/lib/constants/environment.ts
@@ -1,3 +1,8 @@
-export const TOKEN_BACKEND: string = import.meta.env.TOKEN_BACKEND || ""
-export const BACKEND_IMAGE: string = import.meta.env.BACKEND_IMAGE || import.meta.env.VITE_BACKEND_IMAGE || "https://admin.i2tec.ec"
-export const BACKEND_URL: string = import.meta.env.BACKEND_URL || import.meta.env.VITE_BACKEND_URL || "https://admin.i2tec.ec"
+export const BACKEND_IMAGE: string =
+  import.meta.env.BACKEND_IMAGES ||
+  import.meta.env.VITE_BACKEND_IMAGES ||
+  "https://admin.i2tec.ec";
+export const BACKEND_URL: string =
+  import.meta.env.BACKEND_URL ||
+  import.meta.env.VITE_BACKEND_URL ||
+  "https://admin.i2tec.ec";
