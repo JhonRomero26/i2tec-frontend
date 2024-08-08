21509760b4359aa29d28c63721acf4695a5a9c5a
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2024-08-08 15:39:58 -0500

    refactor: remove postcss plugins and update dependencies

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,6 @@
 export default {
   plugins: {
-    "tailwindcss/nesting": "postcss-nested",
     tailwindcss: {},
     "@csstools/postcss-sass": {},
-    "postcss-lightningcss": {
-      browser: ">= .25%",
-      lightningcssOptions: {
-        minify: true,
-      },
-    },
   },
 }
